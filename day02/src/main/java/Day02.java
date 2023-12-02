944b625df838c45a47044efcabf7237e76a2cc9c
Mikael FRECHE <mikael.freche@equasens.com>
2023-12-02 22:24:00 +0100

    day2

--- day02/src/main/java/Day02.java
+++ day02/src/main/java/Day02.java
@@ -29,69 +29,63 @@ public class Day02 extends AbstractMultiStepDay<Long, Long> {
     long result1;
     long result2;
 
+    static final int MAX_GREEN = 13;
+    static final int MAX_RED = 12;
+    static final int MAX_BLUE = 14;
+
     public void readFile() throws IOException {
         try (BufferedReader br = getReader(this.getClass())) {
             String line = br.readLine();
             while (line != null) {
-                
-                computeStep1(line);
-                computeStep2(line);
+                computeResult(line);
 
                 line = br.readLine();
             }
         }
     }
 
-    static final int MAX_GREEN = 13;
-    static final int MAX_RED = 12;
-    static final int MAX_BLUE = 14;
-
-    private void computeStep1(String line) {
-        String[] splitted = line.split(" ");
-        int gameId = Integer.parseInt(splitted[1].replace(":", ""));
-        for (int i = 2; i < splitted.length ; i+=2) {
-            int count = Integer.parseInt(splitted[i]);
-            Color color = Color.valueOf(splitted[i+1]
-                    .replace(",","")
-                    .replace(";","")
-                    .toUpperCase());
-            switch (color) {
-                case RED -> {
-                    if (count > MAX_RED) {
-                        return;
-                    }
-                }
-                case BLUE -> {
-                    if (count > MAX_BLUE) {
-                        return;
-                    }
-                }
-                case GREEN -> {
-                    if (count > MAX_GREEN) {
-                        return;
-                    }
-                }
-            }
-        }
-        result1 += gameId;
-    }
-
-    private void computeStep2(String line) {
+    private void computeResult(String line) {
         String[] splitted = line.split(" ");
         int gameId = Integer.parseInt(splitted[1].replace(":", ""));
         Map<Color, Integer> map = new HashMap<>();
+        boolean keepLineStep1 = true;
         for (int i = 2; i < splitted.length ; i+=2) {
             int count = Integer.parseInt(splitted[i]);
             Color color = Color.valueOf(splitted[i+1]
                     .replace(",","")
                     .replace(";","")
                     .toUpperCase());
+            if(keepLineStep1) {
+                keepLineStep1 = shouldKeepLineStep1(color, count);
+            }
             map.merge(color, count, Integer::max);
+        }
+        if (keepLineStep1) {
+            result1 += gameId;
         }
         long lineResult = (long) map.get(Color.RED) * map.get(Color.BLUE) * map.get(Color.GREEN);
         result2 += lineResult;
     }
 
-
+    private boolean shouldKeepLineStep1(Color color, int count) {
+        switch (color) {
+            case RED -> {
+                if (count > MAX_RED) {
+                    return false;
+                }
+            }
+            case BLUE -> {
+                if (count > MAX_BLUE) {
+                    return false;
+                }
+            }
+            case GREEN -> {
+                if (count > MAX_GREEN) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
