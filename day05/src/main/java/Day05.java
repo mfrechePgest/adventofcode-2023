61e0e8f6dd7a54d4273c69de6da5083d021da4e9
Mikael FRECHE <mikael.freche@equasens.com>
2023-12-05 09:31:59 +0100

    Day05 step 1

---
+++ day05/src/main/java/Day05.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day05 extends AbstractMultiStepDay<Long, Long> {
+
+    public Day05(String fileName) {
+        super(fileName);
+    }
+
+    public Day05() {
+        super("input.txt");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Day05 day05 = new Day05();
+        day05.fullRun();
+    }
+
+    public Long resultStep1() {
+        Stream<Long> stream = mySeeds.stream();
+        for (AlmanacMap map : maps ) {
+            stream = stream
+                    .peek(l -> System.out.println("Applying " + map.label + " on " + l))
+                    .map(map.function)
+                    .peek(l -> System.out.println("Result of " + map.label + " = " + l));
+        }
+        return stream.min(Long::compare).orElse(-1L);
+    }
+
+    public Long resultStep2() {
+        return 0L;
+    }
+
+    private Set<Long> mySeeds = new HashSet<>();
+    private List<AlmanacMap> maps = new ArrayList<>();
+
+
+    public void readFile() throws IOException {
+        try (BufferedReader br = getReader(this.getClass())) {
+            String line = br.readLine();
+            AlmanacMap currentMap = null;
+            while (line != null) {
+                if (line.startsWith("seeds:")) {
+                    mySeeds = Arrays.stream(line.split(" "))
+                            .skip(1)
+                            .map(Long::parseLong)
+                            .collect(Collectors.toSet());
+                } else if (line.isEmpty() || line.isBlank()) {
+                    if( currentMap != null) {
+                        maps.add(currentMap);
+                    }
+                } else if (!Character.isDigit(line.charAt(0))) {
+                    currentMap = new AlmanacMap(line.split(" ")[0]);
+                } else {
+                    String[] splitted = line.split(" ");
+                    Long destinationStart = Long.parseLong(splitted[0]);
+                    Long sourceStart = Long.parseLong(splitted[1]);
+                    Long range = Long.parseLong(splitted[2]);
+                    Function<Long, Long> previousFunc = currentMap.function;
+                    Function<Long, Long> newFunc = i -> {
+                        if (i >= sourceStart && i < sourceStart + range) {
+                            return i + (destinationStart - sourceStart);
+                        } else {
+                            return previousFunc.apply(i);
+                        }
+                    };
+                    currentMap.function = newFunc;
+                }
+                line = br.readLine();
+            }
+            maps.add(currentMap);
+        }
+    }
+
+
+}
