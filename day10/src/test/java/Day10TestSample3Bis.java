493a44ce54b0074b88db145feaaa4c885f885a7f
Mikael FRECHE <mikael.freche@equasens.com>
2023-12-10 23:12:43 +0100

    chore: Nouvelle classe de test quand bcp de samples

--- day10/src/test/java/Day10TestSample3Bis.java
+++
@@ -1,9 +0,0 @@
-public class Day10TestSample3Bis extends AbstractMultiStepDayTest<Day10, Long, Long> {
-
-    public static final String SAMPLE_FILE = "sample3bis.txt";
-
-    public Day10TestSample3Bis() {
-        super(() -> new Day10(SAMPLE_FILE), 22L, 4L);
-    }
-
-}
